f13fc99b0f5980637c26583b09585f1581676cdf
メ ᎡᴏᏞᴇꪎ—͟͞͞ <86321236+sachin9742s@users.noreply.github.com>
2022-09-15 20:10:42 +0530

    Update pm_filter.py

--- PrabhasBotV3/pm_filter.py
+++ PrabhasBotV3/pm_filter.py
@@ -571,10 +571,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
         f_caption = files.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
-                buttons = [[
-                   InlineKeyboardButton(text=f"{query.message.chat.title}", url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-                  ]]
+                f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
+                                                       file_size='' if size is None else size,
+                                                       file_caption='' if f_caption is None else f_caption)
             except Exception as e:
                 logger.exception(e)
                 f_caption = f_caption
