1d5c6a18830b4d2ba50683cdc3e673068084cbf5
メ ᎡᴏᏞᴇꪎ—͟͞͞ <86321236+sachin9742s@users.noreply.github.com>
2022-09-17 08:30:44 +0530

    Update pm_filter.py

--- PrabhasBotV3/pm_filter.py
+++ PrabhasBotV3/pm_filter.py
@@ -842,6 +842,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "nihu":
         buttons = [[
         InlineKeyboardButton('✨ ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ᴍᴏʀᴇ ʙᴜᴛᴛᴏɴs ✨', callback_data='start')
+   ],[
         InlineKeyboardButton('ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
    ],[
         InlineKeyboardButton('🍁 ᴏᴡɴᴇʀ', callback_data='me'),      
