f81981600ba9f5316cb7a715374fe5dde4e2bfcc
メ ᎡᴏᏞᴇꪎ—͟͞͞ <86321236+sachin9742s@users.noreply.github.com>
2022-09-18 16:39:42 +0530

    Update pm_filter.py

--- PrabhasBotV3/pm_filter.py
+++ PrabhasBotV3/pm_filter.py
@@ -841,14 +841,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "nihu":
         buttons = [[
-        InlineKeyboardButton('✨ ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ᴍᴏʀᴇ ʙᴜᴛᴛᴏɴs ✨', callback_data='start')
-   ],[
-        InlineKeyboardButton('ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-   ],[
-        InlineKeyboardButton('🍁 ᴏᴡɴᴇʀ', callback_data='me'),      
-        InlineKeyboardButton('⚙️ ʜᴇʟᴘ', callback_data='help')
-   ],[
-        InlineKeyboardButton('🔰 ɢᴏ ʙᴀᴄᴋ ᴛᴏ ᴍᴀɪɴ ᴍᴇɴᴜ 🔰', callback_data='nihu')   
+        InlineKeyboardButton('✨ ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ᴍᴏʀᴇ ʙᴜᴛᴛᴏɴs ✨', callback_data='start') 
     ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
